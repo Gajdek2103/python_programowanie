e061cbcd51f7eea07164388280fc131dab31d4d7
gajdzinski <gajdeq@wp.pl>
2020-03-28 13:57:13 +0100

    initial

---
+++ kurs.py
@@ -0,0 +1,35 @@
+print ("siema21")
+# dzielenie modulo
+print (10 % 3)
+# dzielenie całkowite
+print ( 2//3)
+# potegowanie
+print ( 2 ** 3 )
+# pierwisatek
+print (2 ** 0.5)
+
+# liczba zespolona
+print (complex())
+
+# operatory porównania <, >, >=, <=,  == rowne, != rozne
+print ( 2 >= 2 )
+print (1 <= 2)
+print ( 3 == 3 )
+print (3 != 4 )
+
+print ( " = "*40)
+print (1 + True)
+print (1+False)
+
+# true = 1 false = 0 
+print ( 1 == True)
+
+# pokazuje nr identyfikacji klasy
+print(id(1))
+print(id(True))
+
+# sprawdze klase
+print(type(1))
+print(type(True))
+
+
