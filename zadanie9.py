4664d8fc3e7a96fbc86949826bef14e79a5be75a
Gajdek2103 <gajdeq@wp.pl>
2020-03-29 11:53:39 +0200

    Urodzenie

--- zadanie9.py
+++ zadanie9.py
@@ -3,7 +3,7 @@ import datetime
 born = int(input("Podaj rok urodzenia: "))
 current_year = datetime.datetime.now().year
 
-if (current_year - born) > 18:
+if (current_year - born) >= 18:
     print("Jesteś pełnoletni/a")
 else:
     print("Nie jesteś pełnoletni/a")
