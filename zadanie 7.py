ea873715b9af9e25644a751711c14be3d88b13da
Gajdek2103 <gajdeq@wp.pl>
2020-03-29 10:40:35 +0200

    Nowe

--- zadanie 7.py
+++ zadanie 7.py
@@ -1,11 +1,8 @@
 a = int(input("Podaj liczbe: "))
 
-b  = a % 2
-c  = a % 3
+b = a % 2
+c = a % 3
 d = a > 10
 e = 7
 
-print(str(b) and str (c) and str(d) or str(e))
-
-
-
+print(str(b) and str(c) and str(d) or str(e))
