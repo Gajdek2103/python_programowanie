49c747a791c74f6b80f5ce28978aef184a8538c8
Gajdek2103 <gajdeq@wp.pl>
2020-03-29 10:52:49 +0200

    Nowy

--- zadanie8.py
+++ zadanie8.py
@@ -3,5 +3,7 @@ a = int(input("Podaj wymiar a: "))
 b = int(input("podaj wymiar b: "))
 c = int(input("Podaj wymiar h: "))
 
-v = (a*b*c) > 1000
-print(str(v))
+v = a * b * c
+x = v > 1000
+print("Objetosc wynosi: " + str(v))
+print("Objetosc jest wieksza niz 1 litr; " + str(x))
