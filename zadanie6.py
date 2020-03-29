7c763af3248d451c7487b6ab64ca49ecc1c9eb96
Gajdek2103 <gajdeq@wp.pl>
2020-03-29 10:15:21 +0200

    Dodanie1

--- zadanie6.py
+++ zadanie6.py
@@ -1,7 +1,7 @@
-a = int(input("Liczba: "))
+a = int(input("Podaj liczba: "))
 z = a>10
 x = a <= 15
 w = a % 2 <= 0
-print(z)
-print(x)
-print(w)
+print("Liczba większa od 10: " + str(z))
+print("Liczba mniejsza rowna 15: " + str(x))
+print("Liczba podzielna przez 2: " + str(w))
